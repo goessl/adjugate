dc4b3b105a6b8ef01e1a8a6573455a42f0f9f57b
Sebastian Gössl <goessl@student.tugraz.at>
2024-09-17 12:48:01 +0200

    Module included in setup
    closes #1

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'adjugate',
-    version = '0.9.1',
-    description = 'A package for calculating submatricies, minors, adjugate- and cofactor matricies.',
+    version = '0.9.3',
+    description = 'A module for calculating submatricies, minors, adjugate- and cofactor matricies.',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     
@@ -16,13 +16,26 @@ setup(
     license = 'MIT',
     
     url = 'https://github.com/goessl/adjugate',
-    py_modules = ['crandom'],
+    py_modules = ['adjugate'],
     python_requires = '>=3.7',
     install_requires = ['numpy'],
     
     classifiers = [
-      'Programming Language :: Python :: 3.7',
-      'License :: OSI Approved :: MIT License',
-      'Operating System :: OS Independent'
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics'
     ]
 )
